c53f0e0229e3bd80bddb03c87a59de849030030b
naveen <odl_user_207831@udacityhol.onmicrosoft.com>
2022-09-18 17:40:12 +0000

    new chnage

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flaskcicdproject.azurewebsites.net:$PORT/predict
+     -X POST https://flaskappdeployment.azurewebsites.net:443/predict
      #your application name <yourappname>goes here
